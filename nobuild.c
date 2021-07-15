75daf6ab71f9c4803fca48296ecb4ab46b1c6f74
rexim <reximkut@gmail.com>
2021-07-16 00:02:41 +0700

    (#7) Switch to clang and run stress-copy.csv by default

--- nobuild.c
+++ nobuild.c
@@ -7,12 +7,12 @@ int main(int argc, char **argv)
 {
     GO_REBUILD_URSELF(argc, argv);
 
-    // CMD("clang", CFLAGS, "-fsanitize=memory", "-o", "minicel", "src/main.c");
-    CMD("gcc", CFLAGS, "-o", "minicel", "src/main.c");
+    CMD("clang", CFLAGS, "-fsanitize=memory", "-o", "minicel", "src/main.c");
+    // CMD("gcc", CFLAGS, "-o", "minicel", "src/main.c");
 
     if (argc > 1) {
         if (strcmp(argv[1], "run") == 0) {
-            CMD("./minicel", "./csv/sum.csv");
+            CMD("./minicel", "./csv/stress-copy.csv");
         } else if (strcmp(argv[1], "gdb") == 0) {
             CMD("gdb", "./minicel");
         } else {
